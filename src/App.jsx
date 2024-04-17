556b66c0e59f0cd7572844409a79ee1082b15866
dhanushramudri <ramudridhanush@gmail.com>
2024-04-17 20:25:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="div1">This is a div</div>
+    </div>
+  );
+}
+
+export default App;
